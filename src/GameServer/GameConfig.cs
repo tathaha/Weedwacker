dde61489e8f0114ce18ca9e54bbefc9e81a9dd82
Chiraitori <77224364+tathaha@users.noreply.github.com>
2022-12-24 16:41:13 +0700

    Update GameConfig.cs

--- src/GameServer/GameConfig.cs
+++ src/GameServer/GameConfig.cs
@@ -144,10 +144,13 @@ namespace Weedwacker.GameServer
                 public string Title = "Welcome to Weedwacker!";
                 public string Content = "Hi there!\r\nFirst of all, welcome to Weedwacker. If you have any issues, please let us know so that Primrose can help you!";
                 public string Sender = "Chika";
+                
+                /*
                 public Weedwacker.game.mail.Mail.MailItem[] items = {
                 new Weedwacker.game.mail.Mail.MailItem(13509, 1, 1),
                 new Weedwacker.game.mail.Mail.MailItem(201, 99999, 1)
                 };
+                */
             }
         }
 
